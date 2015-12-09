327fd70e4f0e773e36f0c19cb97c695bacb6d7cf
pavan chawda <pchawda.expert@gmail.com>
2015-12-09 17:38:17 +0530

    fixed normal message/message-part creations flow

--- app/controllers/messageparts_controller.rb
+++ app/controllers/messageparts_controller.rb
@@ -11,7 +11,7 @@ class MessagepartsController < ApplicationController
     if @messagepart.save
       redirect_to message_path(@message)
     else
-      render 'new'
+      render 'messages/show', id: @message.id
     end
   end
 
